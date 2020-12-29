377b49f297db7b498652cf8cb69f9a78d782dfcb
Sven Siefert <1921202@stud.hs-mannheim.de>
2020-12-29 10:35:09 +0100

    Kommentar TEST

--- src/main/java/de/hsmannheim/web/HalloWeltApplication.java
+++ src/main/java/de/hsmannheim/web/HalloWeltApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HalloWeltApplication {
 
+	//TEST
 	public static void main(String[] args) {
 		SpringApplication.run(HalloWeltApplication.class, args);
 	}
