56c95a728ecd3c2e890fc5888894b6409862f799
Erik <erik.reiter@stud.hs-mannheim.de>
2020-12-29 10:06:34 +0100

    Created Project
    initial project creation

---
+++ src/main/java/de/hsmannheim/web/HalloWelt.java
@@ -0,0 +1,18 @@
+package de.hsmannheim.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+public class HalloWelt {
+    @Getter
+    @Setter
+    private final long id;
+
+    @Getter
+    @Setter
+    private final String inhalt;
+
+
+}
---
+++ src/test/java/de/hsmannheim/web/HalloWeltApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsmannheim.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalloWeltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
