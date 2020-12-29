9f115d2b657da66f297035e198d3265e08e36843
Erik <erik.reiter@stud.hs-mannheim.de>
2020-12-29 10:44:49 +0100

    Hallo Welt App
     :)

--- src/main/java/de/hsmannheim/web/HalloWeltController.java
+++ src/main/java/de/hsmannheim/web/HalloWeltController.java
@@ -1,2 +1,19 @@
-package de.hsmannheim.web;public class HalloWeltController {
+package de.hsmannheim.web;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HalloWeltController {
+    private static final String vorlage = "Hallo ";
+    private long zaehler;
+
+    @GetMapping("/hallo")
+    public HalloWelt hallo(@RequestParam(value = "name", defaultValue = "Welt") String name){
+        return new HalloWelt(zaehler++, vorlage+name);
+    }
+
+
 }
