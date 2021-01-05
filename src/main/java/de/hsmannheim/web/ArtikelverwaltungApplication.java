af4615c4ab0c365a18fda20709e6ab467335e71e
Erik <erik.reiter@stud.hs-mannheim.de>
2021-01-05 09:24:20 +0100

    Artikelverwaltung vorbereitet

--- src/main/java/de/hsmannheim/web/ArtikelverwaltungApplication.java
+++ src/main/java/de/hsmannheim/web/ArtikelverwaltungApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HalloWeltApplication {
+public class ArtikelverwaltungApplication {
 
 	//TEST2
 	public static void main(String[] args) {
-		SpringApplication.run(HalloWeltApplication.class, args);
+		SpringApplication.run(ArtikelverwaltungApplication.class, args);
 	}
 
 }
