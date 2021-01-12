ae773747aca78b00e1f530433f7e2c7844035662
Erik <erik.reiter@stud.hs-mannheim.de>
2021-01-12 13:53:44 +0100

    Finalisiert
    Feddisch

--- src/main/java/de/hsmannheim/web/artikelverwaltung/ArtikelRepository.java
+++ src/main/java/de/hsmannheim/web/artikelverwaltung/ArtikelRepository.java
@@ -9,14 +9,20 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.TreeMap;
 
-@NoArgsConstructor
+
 public class ArtikelRepository {
+    @Getter
     private static long counter = 1000000;
 
     @Setter
     @Getter
     TreeMap<Long, Artikel> artikelMap = new TreeMap<>();
 
+    public ArtikelRepository(){
+        this.add(new Artikel("Test1", 22.0));
+        this.add(new Artikel("Test2", 27.0));
+        this.add(new Artikel("Test3", 92.0));
+    }
 
     public void add(Artikel artikel) {
         artikelMap.put(counter++, artikel);
